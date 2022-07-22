8237f09ab1c9cc17e83d4fdf01dbb414f8066e06
AryanT7 <85297922+AryanT7@users.noreply.github.com>
2022-07-22 10:10:40 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame obj = new JFrame();
+		GamePlay gameplay = new GamePlay();
+		obj.setBounds(10,10,700,600);
+		obj.setTitle("Brick Breaker");
+		obj.setVisible(true);
+		obj.setResizable(false);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gameplay);
+		obj.setLocationRelativeTo(null);
+	}
+
+}
